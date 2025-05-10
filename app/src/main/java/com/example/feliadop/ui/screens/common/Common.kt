d70e17b3adef039bb255b11ac3f2b2dc92605884
Santiago Lara <santiago.lara.galaz@gmail.com>
2025-05-09 23:40:49 -0400

    feat: Add LoadingProgressIndicator composable
    Introduces a new `LoadingProgressIndicator` composable in the `common` package. This composable displays a `CircularProgressIndicator` centered within a `Box` that fills the available space. This is useful for indicating loading states in various screens of the application.

--- app/src/main/java/com/example/feliadop/ui/screens/common/Common.kt
+++ app/src/main/java/com/example/feliadop/ui/screens/common/Common.kt
@@ -1,9 +1,12 @@
 package com.example.feliadop.ui.screens.common
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.feliadop.ui.theme.FeliAdopTheme
 
@@ -15,5 +18,16 @@ fun Screen(context: @Composable () -> Unit) {
             color = MaterialTheme.colorScheme.background,
             content = context
         )
+    }
+}
+
+@Composable
+fun LoadingProgressIndicator(modifier: Modifier){
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        CircularProgressIndicator()
     }
 }
