873a16466aaa9ea01b6520060d9232c30ed0981c
Santiago Lara <santiago.lara.galaz@gmail.com>
2025-05-13 23:54:08 -0400

    refactor: Relocate Navigation composable
    Moves the `Navigation` composable from the `ui.screens` package to a new `ui.screens.navigation` package. This improves the organization of UI-related code by grouping navigation components together.

--- app/src/main/java/com/example/feliadop/MainActivity.kt
+++ app/src/main/java/com/example/feliadop/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.feliadop.ui.screens.Navigation
+import com.example.feliadop.ui.screens.navigation.Navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
